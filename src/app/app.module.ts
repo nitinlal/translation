0fd803ccd891e3a28de555a5bd2499a2f5d69a20
Nitin Lal <nitin.lal@cigna.com>
2017-03-04 16:45:47 -0800

    wip for translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule, Http} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {} from '@ngx-translate/http-loader';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function httpLoaderFactory(http: Http) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -12,9 +19,17 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TranslateModule.forRoot(
+      {
+        loader: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        dep: [Http]
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
